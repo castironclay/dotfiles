8a1fcc5155f78aea677002f850e1290c14ea38a7
Clay Coppage <clay@horizon3.ai>
2023-12-28 22:52:57 -0500

    Add keymaps'

--- dot_config/nvim/lua/plugins/transparent.lua
+++ dot_config/nvim/lua/plugins/transparent.lua
@@ -1,6 +1,8 @@
 return {
-  "xiyaowong/transparent.nvim",
-  config = function()
-    require("transparent").setup({})
-  end,
+	"xiyaowong/transparent.nvim",
+	config = function()
+		require("transparent").setup({})
+		vim.keymap.set("n", "<leader>te", ":TransparentEnable<cr>", {})
+		vim.keymap.set("n", "<leader>td", ":TransparentDisable<cr>", {})
+	end,
 }
